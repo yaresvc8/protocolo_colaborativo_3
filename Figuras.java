83ae4deb467700d68cc2d86852487d4378972a33
yaresvc8 <yvilladiegoc@unicartagena.edu.co>
2025-11-07 16:08:17 -0500

    ejercicio 1 taller 13

---
+++ Figuras.java
@@ -0,0 +1,47 @@
+abstract class Figura {
+    
+    public abstract double calcularArea();
+}
+
+class Rectangulo extends Figura {
+    private double base;
+    private double altura;
+
+    public Rectangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    @Override
+    public double calcularArea() {
+        return base * altura;
+    }
+}
+
+class Triangulo extends Figura {
+    private double base;
+    private double altura;
+
+    public Triangulo(double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    @Override
+    public double calcularArea() {
+        return (base * altura) / 2;
+    }
+}
+
+public class Figuras {
+    public static void main(String[] args) {
+        
+        Figura rectangulo = new Rectangulo(5, 8);
+        Figura triangulo = new Triangulo(6, 4);
+
+        System.out.println("Área del rectángulo: " + rectangulo.calcularArea());
+        System.out.println("Área del triángulo: " + triangulo.calcularArea());
+    }
+}
+
+
